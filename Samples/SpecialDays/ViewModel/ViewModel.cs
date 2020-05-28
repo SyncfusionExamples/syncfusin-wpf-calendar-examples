8e29f27802de0f817b1f3e035a1f75f7ae489161
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-05-28 12:34:47 +0530

    Blackout and specialdays sample committed.

---
+++ Samples/SpecialDays/ViewModel/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+using Syncfusion.Windows.Shared;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace SpecialDays
+{
+    //public class SpecialDates : SpecialDatesCollection
+    //{
+
+    //}
+
+
+    public class ViewModel : NotificationObject
+    {
+        private SpecialDatesCollection specialDates = new SpecialDatesCollection();
+        public SpecialDatesCollection SpecialDates
+        {
+            get
+            {
+                return specialDates;
+            }
+            set
+            {
+                specialDates = value;
+                this.RaisePropertyChanged(nameof(SpecialDates));
+            }
+        }
+        public ViewModel()
+        {
+        }
+    }
+}
